356b3e7cc0e9efeffba19f046814e8545a9a719e
SeanLim <happylim99@gmail.com>
2019-10-12 17:58:29 +0800

    axios

--- src/main.js
+++ src/main.js
@@ -1,12 +1,48 @@
 import Vue from 'vue'
 import App from './App.vue'
+import axios from 'axios'
+
 import router from './router'
 import store from './store'
 
-Vue.config.productionTip = false
+axios.defaults.baseURL = 'https://vue-axios-78ada.firebaseio.com'
+//set header to every request by using common
+axios.defaults.headers.common['Authorization'] = 'token123'
+//set header to every get request
+axios.defaults.headers.get['Accepts'] = 'application/json'
+
+/*
+axios.interceptors.request.use(config => {
+	console.log('Request interceptor' + config)
+	//config.headers['aaaa'] = ''
+	//return is a must, or the process will stuck
+	return config
+})
+
+axios.interceptors.response.use(res => {
+	console.log('Response interceptor', res)
+	return res
+})
+*/
+
+const reqInterceptor = axios.interceptors.request.use(config => {
+	console.log('Request interceptor' + config)
+	//config.headers['aaaa'] = ''
+	//return is a must, or the process will stuck
+	return config
+})
+
+const resInterceptor = axios.interceptors.response.use(res => {
+	console.log('Response interceptor', res)
+	return res
+})
+
+axios.interceptors.request.eject(reqInterceptor)
+axios.interceptors.response.eject(resInterceptor)
 
 new Vue({
+  el: '#app',
   router,
   store,
   render: h => h(App)
-}).$mount('#app')
+})
